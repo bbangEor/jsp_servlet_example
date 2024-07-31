b0735281ac422b6b349a859407252aaef58a2155
bbangEor <eor1402@naver.com>
2024-07-31 17:04:25 +0900

    first-commit

---
+++ src/main/java/com/tenco/servlet/FilterServlet.java
@@ -0,0 +1,25 @@
+package com.tenco.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class FilterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FilterServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
